8b1a72af5d26a55276f91c43764deebee4eb1f4b
Ivan <justworkkit@gmail.com>
2015-07-28 00:47:43 +0300

    Improve program with "flag"

--- test.go
+++ test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"regexp"
 	"time"
 )
@@ -25,29 +25,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func input() {
 	var s string
-	a := " "
-	for {
-		s = " "
-		if len(os.Args) > 1 {
-			s = os.Args[1]
-		}
-		if s != a {
-			k, y := regexp.MatchString("[0-9]", s)
-			if k == true && y == nil {
-				s = ":" + s
-			}
-			if err3 := http.ListenAndServeTLS(s, "cert.pem", "key.pem", nil); err3 != nil {
-				log.Println("failed to start server", err3)
-				continue
-			} else {
-				break
-			}
+	port := flag.String("port", "9111", "port in server")
+	flag.Parse()
+	s = *port
+	k, y := regexp.MatchString("[0-9]", s)
+	if k == true && y == nil {
+		s = ":" + s
+		if err3 := http.ListenAndServeTLS(s, "cert.pem", "key.pem", nil); err3 != nil {
+			log.Println("failed to start server", err3)
 		} else {
-			if err3 := http.ListenAndServeTLS(":9876", "cert.pem", "key.pem", nil); err3 != nil {
-				log.Println("failed to start server", err3)
-				continue
-			}
+
 		}
+	} else {
+		log.Println("unexpected input", y)
 	}
 }
 
