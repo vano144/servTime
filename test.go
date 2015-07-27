9457bb73786423a3153bcceee749a02587feeb10
Ivan <justworkkit@gmail.com>
2015-07-27 21:08:20 +0300

    remove request from console

--- test.go
+++ test.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"regexp"
 	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	t := time.Now().Local()
-	_, err := fmt.Fprintf(w, "Current time : ")
 	_, err1 := fmt.Fprintf(w, t.Format("2006-01-02 15:04:05"))
-	if err != nil || err1 != nil {
+	if err1 != nil {
 		log.Fatal("failed to write", err, err1)
 	}
 	defer func() {
@@ -24,10 +24,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func input() {
 	var s string
+	a := " "
 	for {
-		fmt.Println("Please input port")
-		_, err2 := fmt.Scanf("%s", &s)
-		if err2 == nil {
+		s = " "
+		if len(os.Args) > 1 {
+			s = os.Args[1]
+		}
+		if s != a {
 			k, y := regexp.MatchString("[0-9]", s)
 			if k == true && y == nil {
 				s = ":" + s
@@ -39,7 +42,7 @@ func input() {
 				break
 			}
 		} else {
-			if err3 := http.ListenAndServeTLS(s, "cert.pem", "key.pem", nil); err3 != nil {
+			if err3 := http.ListenAndServeTLS(":9876", "cert.pem", "key.pem", nil); err3 != nil {
 				log.Println("failed to start server", err3)
 				continue
 			}
