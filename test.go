e7a32359f83de6d7d52d513016e7aa8de291b946
Ivan <justworkkit@gmail.com>
2015-07-27 19:27:46 +0300

    Web-server, which shows time/ may input number of port

---
+++ test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().Local()
+	_, err := fmt.Fprintf(w, "Current time : ")
+	_, err1 := fmt.Fprintf(w, t.Format("2006-01-02 15:04:05"))
+	if err != nil || err1 != nil {
+		log.Fatal("failed to write", err, err1)
+	}
+	defer func() {
+		if x := recover(); x != nil {
+			log.Printf("%s", x, "caught panic")
+		}
+	}()
+}
+
+func input() {
+	var s string
+	for {
+		fmt.Println("Please input port")
+		_, err2 := fmt.Scanf("%s", &s)
+		if err2 == nil {
+			s = ":" + s
+			if err3 := http.ListenAndServeTLS(s, "cert.pem", "key.pem", nil); err3 != nil {
+				log.Println("failed to start server", err3)
+				continue
+			} else {
+				break
+			}
+		} else {
+			log.Println("problem with input", err2)
+			continue
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/gettime", handler)
+	input()
+}
