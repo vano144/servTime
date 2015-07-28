04bd45a53e18196ea3b7876a3c1a11962403e8ff
Ivan <justworkkit@gmail.com>
2015-07-28 09:07:13 +0300

    Delete useless code

--- test.go
+++ test.go
@@ -5,43 +5,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"regexp"
 	"time"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	const layout = "Jan 2, 2006 at 3:04pm (MST)"
 	t := time.Now()
-	_, err1 := fmt.Fprintf(w, t.UTC().Format(layout))
-	if err1 != nil {
+	if _, err1 := fmt.Fprintf(w, t.UTC().Format(layout)); err1 != nil {
 		log.Fatal("failed to write", err1)
 	}
-	defer func() {
-		if x := recover(); x != nil {
-			log.Printf("%s", x, "caught panic")
-		}
-	}()
 }
 
-func input() {
-	var s string
-	port := flag.String("port", "9111", "port in server")
+func handlerCMDArgs() {
+	port := flag.String("port", ":9111", "port in server")
 	flag.Parse()
-	s = *port
-	k, y := regexp.MatchString("[0-9]", s)
-	if k == true && y == nil {
-		s = ":" + s
-		if err3 := http.ListenAndServeTLS(s, "cert.pem", "key.pem", nil); err3 != nil {
-			log.Println("failed to start server", err3)
-		} else {
-
-		}
-	} else {
-		log.Println("unexpected input", y)
+	if err3 := http.ListenAndServeTLS(*port, "cert.pem", "key.pem", nil); err3 != nil {
+		log.Fatal("failed to start server", err3)
 	}
 }
 
 func main() {
 	http.HandleFunc("/time", handler)
-	input()
+	handlerCMDArgs()
 }
